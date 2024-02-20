547fc64a13de8ca9a58b6edbae8af40b340bd9b6
Alice Marique <mariquealice83@gmail.com>
2024-02-20 16:55:17 +0100

    deplacer etudiant

--- cas_utilisation4/etudiant.php
+++ cas_utilisation4/etudiant.php
@@ -26,8 +26,92 @@ class Groupe {
         $this->etudiants = $etudiants;
     }
 
+    public function getEtudiants(): array {
+        return $this->etudiants;
+    }
+
+    public function scoreMoyen(): float {
+        $total = 0;
     
+        foreach ($this->etudiants as $etudiant) {
+            $total += $etudiant->getNote();
+        }
+    
+        return $total / count($this->etudiants);
+    }
+
+    public function ajouterEtudiant(Etudiant $etudiant) {
+        $this->etudiants[] = $etudiant;
+    }
+
+    public function deplacerEtudiant(string $nomEtudiant, Groupe $groupeChange) {
+        foreach ($this->etudiants as $index => $etudiant) {
+            if ($etudiant->getNom() === $nomEtudiant) {
+                $groupeChange->ajouterEtudiant($etudiant);
+                unset($this->etudiants[$index]);
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+$etudiantsGroupe1 = [
+    new Etudiant("Jagen", 18),
+    new Etudiant("Sophie", 16),
+    new Etudiant("Camille", 15),
+    new Etudiant("Antoine", 14),
+    new Etudiant("Julie", 17),
+    new Etudiant("Marie", 19),
+    new Etudiant("Luc", 20),
+    new Etudiant("Emma", 13),
+    new Etudiant("Nicolas", 13),
+    new Etudiant("Thomas", 12)
+];
+
+$etudiantsGroupe2 = [
+    new Etudiant("Mathieu", 14),
+    new Etudiant("Elodie", 13),
+    new Etudiant("Vincent", 15),
+    new Etudiant("Clément", 18),
+    new Etudiant("Pauline", 16),
+    new Etudiant("Alexandre", 13),
+    new Etudiant("Caroline", 19),
+    new Etudiant("Julien", 13),
+    new Etudiant("Aurélie", 11),
+    new Etudiant("Emilie", 20)
+];
 
 
+$groupe1 = new Groupe($etudiantsGroupe1);
+$groupe2 = new Groupe($etudiantsGroupe2);
 
+
+$scoreMoyenGroupe1 = $groupe1->scoreMoyen();
+$scoreMoyenGroupe2 = $groupe2->scoreMoyen();
+
+echo "Étudiants du groupe 1 après le déplacement : <br>";
+foreach ($groupe1->getEtudiants() as $etudiant) {
+    echo $etudiant->getNom() . "<br>";
+}
+
+echo "Étudiants du groupe 2 après le déplacement : <br>";
+foreach ($groupe2->getEtudiants() as $etudiant) {
+    echo $etudiant->getNom() . "<br>";
+}
+
+echo "Score moyen du groupe 1 : " . $scoreMoyenGroupe1 . "<br>";
+echo "Score moyen du groupe 2 : " . $scoreMoyenGroupe2 . "<br>";
+
+$groupe1->deplacerEtudiant("Jagen", $groupe2);
+$groupe2->deplacerEtudiant("Vincent", $groupe1);
+
+echo "Étudiants du groupe 1 après le déplacement : <br>";
+foreach ($groupe1->getEtudiants() as $etudiant) {
+    echo $etudiant->getNom() . "<br>";
+}
+
+echo "Étudiants du groupe 2 après le déplacement : <br>";
+foreach ($groupe2->getEtudiants() as $etudiant) {
+    echo $etudiant->getNom() . "<br>";
 }

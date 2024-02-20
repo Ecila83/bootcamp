59f92534a16c5bdcda1b15b9d02dc452e3012b84
Alice Marique <mariquealice83@gmail.com>
2024-02-20 15:14:05 +0100

    tableau fini

--- cas_d'utilisation3/web.php
+++ cas_d'utilisation3/web.php
@@ -22,13 +22,20 @@ class Contenu {
 Class Article extends Contenu {
     const BREAKINGNEWS = "Breaking News";
     const BREAKING = "BREAKING : ";
+
+    public $titreSuivant;
+
+    public function __construct(string $titre, string $texte, string $titreSuivant = null) {
+        parent::__construct($titre, $texte);
+        $this->titreSuivant = $titreSuivant;
+    }
        
-    public function titreArticle($titreSuivant){
+    public function titreArticle(){
        
-        if($this->titre === self::BREAKINGNEWS){
-            return "<h2>".self::BREAKING.$titreSuivant."</h2>";
+        if($this->titre === self::BREAKINGNEWS && $this->titreSuivant !== null){
+            return "<h2>".self::BREAKING.$this->titreSuivant."</h2>";
         }else{
-            return "<h2>" .$this -> titre."</h2>";
+            return parent::afficherTitre();
         }
     }
 
@@ -52,27 +59,17 @@ $contenus = [];
 
 
 $contenus[] = new Article("Les avantages de la technologie moderne", "La technologie moderne a révolutionné notre façon de vivre et de travailler. Dans cet article, nous explorons les nombreux avantages de l'utilisation de la technologie dans différents aspects de notre vie quotidienne.");
-$contenus[] = new Article("Breaking News", "Une nouvelle importante vient de tomber. Restez à l'écoute pour plus de détails.");
+$contenus[] = new Article("Breaking News", "Une nouvelle importante vient de tomber. Restez à l'écoute pour plus de détails.", "Fantastic!");
 
 $contenus[] = new Annonce("Offre spéciale : Vente Flash !", "Ne manquez pas notre vente flash exclusive ! Des réductions incroyables sur une large gamme de produits. Faites vite, l'offre se termine bientôt !");
 
 $contenus[] = new PosteVacant("Développeur Web Full Stack recherché", "Nous recherchons un développeur Web Full Stack talentueux pour rejoindre notre équipe dynamique. Si vous êtes passionné par la création de sites Web et que vous avez une expérience pratique avec les technologies de développement Web.");
 
-
-$titresSuivants = [
-    "Breaking News" => "Fantastique!"
-];
-
 foreach ($contenus as $contenu) {
-    if ($contenu instanceof Article && $contenu->titre === Article::BREAKINGNEWS) {
-      
-        if (isset($titresSuivants[$contenu->titre])) {
-            echo $contenu->titreArticle($titresSuivants[$contenu->titre]);
-        } else {
-            echo $contenu->afficherTitre(); 
-        }
+    if ($contenu instanceof Article) {
+        echo $contenu->titreArticle(); 
     } else {
-        echo $contenu->afficherTitre();
+        echo $contenu->afficherTitre(); 
     }
     echo $contenu->afficherTexte();
 }

3b673ca77c8072b7c5c53f8d19055bf8a10941db
Alice Marique <mariquealice83@gmail.com>
2024-02-20 10:52:22 +0100

    utilisatio 1 avec class

---
+++ cas_d'utilisation1/avecclass.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+class panier{
+    const PRIX_BANANE = 1;
+    const PRIX_POMME = 1.50;
+    const PRIX_VIN = 10;
+    const TAXE6 = 0.06;
+    const TAXE21 = 0.21;
+
+    public $quantiteBanane;
+    public $quantitePomme;
+    public $quantiteVin;
+
+    public function __construct(int $quantiteBanane, int $quantitePomme, int $quantiteVin){
+        $this->quantiteBanane = $quantiteBanane;
+        $this->quantitePomme = $quantitePomme;
+        $this->quantiteVin = $quantiteVin;
+    }
+
+    public function prix(){
+        $prix = $this->quantiteBanane * self::PRIX_BANANE +
+                $this->quantitePomme * self::PRIX_POMME +
+                $this->quantiteVin * self::PRIX_VIN;
+        return $prix;
+    }
+
+    public function taxe(){
+        $taxe6 = ($this->quantiteBanane * self::PRIX_BANANE + $this->quantitePomme * self::PRIX_POMME) * self::TAXE6;
+        $taxe21 = $this->quantiteVin * self::PRIX_VIN * self::TAXE21;
+        return $taxe6 + $taxe21;
+    }
+
+    public function Htva(){
+        $prixHtva = $this->prix()-$this->taxe();
+        return $prixHtva;
+    }
+}
+
+$panier = new panier(6, 3, 2);
+echo "Prix total: " . $panier->prix() . "<br>";
+echo "Taxe totale: " . $panier->taxe() . "<br>";
+echo "prix Htva: " . $panier->Htva() . "<br>";
+
